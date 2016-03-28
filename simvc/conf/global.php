e115eaf6cb2821a7e36169817ed6d34cae9d7fbe
yiyide266 <yiyiyide@gmail.com>
2016-03-28 21:56:47 +0800

    Create global.php

---
+++ simvc/conf/global.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+    'sys_db_host' => '192.168.88.207',
+	'sys_db_user' => 'root',
+	'sys_db_pass' => '',
+	'sys_db_name' => 'dbmark',
+	'sys_db_charset' => 'utf8',
+
+
+
+	'url_type' => 0,
+	'url_entr' => '',
+	'file_cache' => true,
+	'file_cache_life_time' => 5,
+	'sys_request_log' => false,
+	'sys_user_expire' => 3600
+);
+
+
+
+?>
