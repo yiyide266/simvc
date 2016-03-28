97188db6585e21a0e4146933f7b569f54625982c
yiyide266 <yiyiyide@gmail.com>
2016-03-28 22:02:06 +0800

    Create Config.class.php

---
+++ simvc/lib/config/Config.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace simvc\lib\config;
+abstract class Config{
+    protected static $instance = null;
+    protected $config = array();
+    
+    abstract public function get( $key );
+    abstract public function set( $key,$value );
+}
+?>
