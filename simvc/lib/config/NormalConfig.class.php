31b061326088616420600fc18d8ef5b3ee5368fa
yiyide266 <yiyiyide@gmail.com>
2016-03-28 22:04:46 +0800

    Create NormalConfig.class.php

---
+++ simvc/lib/config/NormalConfig.class.php
@@ -0,0 +1,25 @@
+<?php
+namespace simvc\lib\config;
+class NormalConfig extends Config{
+
+    private static $path = "simvc/conf/global.php";
+	public static function instance(){
+		if( is_null( self::$instance ) ){
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+	public function __construct(){
+		//echo "config initialize<br>";
+		$this -> config =  include(  _DIR_.'/'.self::$path  );
+		//var_dump( $this -> config );
+	}
+	
+	public function get( $key ){
+		return !isset($this -> config[$key])?null:$this -> config[$key];
+	}
+	public function set( $key,$value ){
+		$this -> config[$key] = $value;
+	}
+}
+?>
