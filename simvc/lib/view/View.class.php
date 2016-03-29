4da32aef60ab241a15facb6c81d7674dfddeb609
yiyide266 <yiyiyide@gmail.com>
2016-03-29 23:25:05 +0800

    Update View.class.php

--- simvc/lib/view/View.class.php
+++ simvc/lib/view/View.class.php
@@ -15,7 +15,7 @@ class View{
 	
 	
 	public function display( $tpl = null ){
-		$file = is_null($tpl)?_APP_.'/'.req(0,"a")."/view/".req(0,"c")."/".req(0,"m").".php":_APP_.'/'.req(0,"app")."/view/".$tpl;
+		$file = is_null($tpl)?_APP_.'/'.req(0,"a")."/view/".req(0,"c")."/".req(0,"m").".php":_APP_.'/'.req(0,"a")."/view/".$tpl;
 		//var_dump($file);exit;
 		if( !is_file($file) ){
 			throw new Exception("View file not exits");
