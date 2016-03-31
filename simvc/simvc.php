36d9152c7ff3f918afc89f01a4003c8605aac9ec
yiyide266 <yiyiyide@gmail.com>
2016-03-31 17:50:10 +0800

    Update simvc.php

--- simvc/simvc.php
+++ simvc/simvc.php
@@ -6,7 +6,11 @@ class simvc{
 
 
 	public function run(){
+		//register_shutdown_function('simvc\simvc::fatalError');
+		set_error_handler ('simvc\simvc::error');
+		set_exception_handler ( 'simvc\simvc::exception' );
 		spl_autoload_register('simvc\simvc::autoload');
+		//asdsad;
 		//var_dump($_SERVER);exit;
 		/*var_dump(req_get_all());
 		echo '<br>';
@@ -20,13 +24,41 @@ class simvc{
 		echo '<br>';*/
 		$c = "\\"._APP."\\".req(0,"a")."\\controller\\".req(0,"c")."Controller";
 		//var_dump($c);
+		
 		$handler = new lib\controller\CacheDecorator(  new $c()  );
+		
 		$handler -> run( req(0,"m") );
 		
 	}
 
 	public static function autoload( $class ){
 		require_once( strtr($class.self::EXT, "\\","/") );
+	}
+
+	/*public static function fatalError() {
+		$status['error_get_last'] = error_get_last();
+		handle_err( 0, $status );
+    }*/
+
+	public static function error( $errno ,  $errstr ,  $errfile ,  $errline  ){
+		//var_dump($errstr);
+		//new lib\exception\ErrorHandler ( $errno ,  $errstr ,  $errfile ,  $errline  );
+		$status['errno'] = $errno;
+		$status['errstr'] = $errstr;
+		$status['errfile'] = $errfile;
+		$status['errline'] = $errline;
+		handle_err( 1, $status );
+	}
+
+	public static function exception( $e ){
+		//var_dump( $e );
+		//new lib\exception\ExceptionHandler( $e );
+		$status['message'] = $e -> getMessage ();
+		$status['code'] = $e -> getCode ();
+		$status['file'] = $e -> getFile ();
+		$status['line'] = $e -> getLine ();
+		$status['trace'] = addslashes($e -> getTraceAsString ());
+		handle_err( 2, $status );
 	}
 
 }
