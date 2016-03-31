07b24b552f9ea9d1da5859243dba4f6c4dc66ce9
yiyide266 <yiyiyide@gmail.com>
2016-03-31 17:52:51 +0800

    Update init.php

--- simvc/inc/init.php
+++ simvc/inc/init.php
@@ -4,7 +4,10 @@ define( '_CONFIG', 'Normal' );
 define( '_APP', 'app' );
 define( '_APP_', _DIR_.'/'._APP );
 define( '_PUBLIC_', _DIR_.'/public' );
+define( '_ERROR_TEMPLATE_HTML', _PUBLIC_.'/templates/error_template_html.html' );
+define( '_ERROR_TEMPLATE_JSON', _PUBLIC_.'/templates/error_template_json.html' );
 
+define( '_PURL_', '/public' );
 
-
+define('IS_AJAX',       ((isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') ) ? true : false);
 ?>
