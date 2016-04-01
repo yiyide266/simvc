dd1fc08a6153bf2d89d509d5f16d046e7611f8bd
yiyide266 <yiyiyide@gmail.com>
2016-04-01 14:54:16 +0800

    Update simvc.php

--- simvc/simvc.php
+++ simvc/simvc.php
@@ -10,18 +10,7 @@ class simvc{
 		set_error_handler ('simvc\simvc::error');
 		set_exception_handler ( 'simvc\simvc::exception' );
 		spl_autoload_register('simvc\simvc::autoload');
-		//asdsad;
-		//var_dump($_SERVER);exit;
-		/*var_dump(req_get_all());
-		echo '<br>';
-		var_dump(a_uri());
-		echo '<br>';
-		var_dump(c_uri());
-		echo '<br>';
-		var_dump(m_uri());
-		echo '<br>';
-		var_dump(f_uri());
-		echo '<br>';*/
+		
 		$c = "\\"._APP."\\".req(0,"a")."\\controller\\".req(0,"c")."Controller";
 		//var_dump($c);
 		
@@ -41,8 +30,7 @@ class simvc{
     }*/
 
 	public static function error( $errno ,  $errstr ,  $errfile ,  $errline  ){
-		//var_dump($errstr);
-		//new lib\exception\ErrorHandler ( $errno ,  $errstr ,  $errfile ,  $errline  );
+		
 		$status['errno'] = $errno;
 		$status['errstr'] = $errstr;
 		$status['errfile'] = $errfile;
@@ -51,13 +39,12 @@ class simvc{
 	}
 
 	public static function exception( $e ){
-		//var_dump( $e );
-		//new lib\exception\ExceptionHandler( $e );
+		
 		$status['message'] = $e -> getMessage ();
 		$status['code'] = $e -> getCode ();
 		$status['file'] = $e -> getFile ();
 		$status['line'] = $e -> getLine ();
-		$status['trace'] = addslashes($e -> getTraceAsString ());
+		$status['trace'] = $e -> getTraceAsString ();
 		handle_err( 2, $status );
 	}
 
