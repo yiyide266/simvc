f1fab02245072a6cf2524af4c679124d2a98051d
yiyide266 <yiyiyide@gmail.com>
2016-04-10 01:27:31 +0800

    Update addPage.php

--- app/rbac/view/Roles/addPage.php
+++ app/rbac/view/Roles/addPage.php
@@ -2,15 +2,15 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>ÎÞ±êÌâÎÄµµ</title>
+<title>roles add</title>
 </head>
 <body>
 
 <form action="<?php echo assem_m( 'add' );?>" method="post">
-	½M  Ãû£º<input type='text' name='r_name'></input><br>
-	½MÃèÊö£º<input type='text' name='r_des'></input><br>
+	r_name:<input type='text' name='r_name'></input><br>
+	r_des:<input type='text' name='r_des'></input><br>
 	<input type='hidden' name='form_token' value="<?php echo $this -> val['token']?>">
-	<input type="submit" value="Ìá½»">
+	<input type="submit" value="submit">
 </form>
 
 </body>
