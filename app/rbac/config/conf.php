12d5254def926c75ee5a5158eaf77503910458a1
yiyide266 <yiyiyide@gmail.com>
2016-04-22 17:19:32 +0800

    Update conf.php

--- app/rbac/config/conf.php
+++ app/rbac/config/conf.php
@@ -1,9 +1,12 @@
 <?php
 return array(
-	'sys_db_host' => '192.168.255.131',
-	'sys_db_pass' => '123456',
+	'sys_db_host' => '192.168.88.207',
+	'sys_db_pass' => '',
 	'sys_db_name' => 'simvc',
 	'sys_db_prefix' => 'sim_',
+
+
+	'url_type' => 1,
 
 	'test' => 'config extends',
 
