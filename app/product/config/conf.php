ef106e214859eb98de9eeae3076ca56ec7b625ea
yiyide266 <yiyiyide@gmail.com>
2016-05-19 10:34:41 +0800

    Create conf.php

---
+++ app/product/config/conf.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	'sys_db_host' => '192.168.88.207',
+	'sys_db_pass' => '',
+	'sys_db_name' => 'simvc',
+	'sys_db_prefix' => 'sim_',
+
+
+	'url_type' => 1,
+
+	'test' => 'config extends',
+
+	);
+
+
+
+?>
